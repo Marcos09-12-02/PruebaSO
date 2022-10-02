aca7586c085a3d9d76a9779930730fcd3e77833f
Marcos09-12-02 <marcos.carmona@estudiantat.upc.edu>
2022-10-02 18:03:13 +0200

    Nombre en mayusculas

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -44,9 +44,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 47);
+            this.label1.Location = new System.Drawing.Point(46, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 25);
+            this.label1.Size = new System.Drawing.Size(46, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "IP";
             // 
@@ -54,32 +55,37 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 25);
+            this.label2.Location = new System.Drawing.Point(34, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.Size = new System.Drawing.Size(156, 37);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Nombre";
+            this.label2.Text = "NOMBRE";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // IP
             // 
-            this.IP.Location = new System.Drawing.Point(84, 52);
+            this.IP.Location = new System.Drawing.Point(126, 80);
+            this.IP.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.IP.Name = "IP";
-            this.IP.Size = new System.Drawing.Size(130, 20);
+            this.IP.Size = new System.Drawing.Size(193, 26);
             this.IP.TabIndex = 2;
             this.IP.Text = "192.168.56.102";
             // 
             // nombre
             // 
-            this.nombre.Location = new System.Drawing.Point(116, 31);
+            this.nombre.Location = new System.Drawing.Point(174, 48);
+            this.nombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.nombre.Name = "nombre";
-            this.nombre.Size = new System.Drawing.Size(164, 20);
+            this.nombre.Size = new System.Drawing.Size(244, 26);
             this.nombre.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(287, 46);
+            this.button1.Location = new System.Drawing.Point(430, 71);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 31);
+            this.button1.Size = new System.Drawing.Size(126, 48);
             this.button1.TabIndex = 4;
             this.button1.Text = "conectar";
             this.button1.UseVisualStyleBackColor = true;
@@ -87,9 +93,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(131, 142);
+            this.button2.Location = new System.Drawing.Point(196, 218);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 5;
             this.button2.Text = "Enviar";
             this.button2.UseVisualStyleBackColor = true;
@@ -103,9 +110,11 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.nombre);
-            this.groupBox1.Location = new System.Drawing.Point(8, 207);
+            this.groupBox1.Location = new System.Drawing.Point(12, 318);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(363, 202);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(544, 311);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Peticion";
@@ -113,9 +122,10 @@
             // Longitud
             // 
             this.Longitud.AutoSize = true;
-            this.Longitud.Location = new System.Drawing.Point(116, 91);
+            this.Longitud.Location = new System.Drawing.Point(174, 140);
+            this.Longitud.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Longitud.Name = "Longitud";
-            this.Longitud.Size = new System.Drawing.Size(166, 17);
+            this.Longitud.Size = new System.Drawing.Size(247, 24);
             this.Longitud.TabIndex = 7;
             this.Longitud.TabStop = true;
             this.Longitud.Text = "Dime la longitud de mi nombre";
@@ -124,9 +134,10 @@
             // Bonito
             // 
             this.Bonito.AutoSize = true;
-            this.Bonito.Location = new System.Drawing.Point(116, 68);
+            this.Bonito.Location = new System.Drawing.Point(174, 105);
+            this.Bonito.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Bonito.Name = "Bonito";
-            this.Bonito.Size = new System.Drawing.Size(156, 17);
+            this.Bonito.Size = new System.Drawing.Size(233, 24);
             this.Bonito.TabIndex = 8;
             this.Bonito.TabStop = true;
             this.Bonito.Text = "Dime si mi nombre es bonito";
@@ -134,13 +145,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(741, 562);
+            this.ClientSize = new System.Drawing.Size(1112, 865);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.IP);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -94,6 +94,9 @@ namespace WindowsFormsApplication1
 
         }
 
-     
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
